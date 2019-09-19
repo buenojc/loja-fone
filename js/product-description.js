05d0e5a71514170d0851866aca936c69b0d7c88b
Julio Cesar Bueno <buenojc@outlook.com>
2019-09-18 23:16:00 -0300

    [014] Desafio Intermediário Finalizado

--- js/product-description.js
+++ js/product-description.js
@@ -1,7 +1,14 @@
 const $heart = window.document.querySelector(".heart");
-
-$heart.addEventListener("click", handleClick);
+const $stars = document.querySelectorAll(".star");
 
 function handleClick() {
   $heart.classList.toggle("-active");
+}
+
+$heart.addEventListener("click", handleClick);
+
+for (let i = 0; i < $stars.length; i++) {
+  $stars[i].addEventListener("click", () => {
+    $stars[i].classList.toggle("-marked");
+  });
 }
